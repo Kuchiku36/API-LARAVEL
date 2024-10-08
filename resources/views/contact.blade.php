6b2bbaca800b64172d7fed8ac1a476150a6d56d2
veronique derick <derickveronique@gmail.com>
2024-10-08 13:56:27 -0400

    contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,17 +1,105 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('contact') }}
+            {{ __('Contact') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-100">Contact Us</h1>
+
+                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div class="sm:col-span-3">
+                            <label for="first-name" class="block text-sm font-light leading-6 text-gray-900">First
+                                name *</label>
+                            <div class="mt-2">
+                                <input type="text" name="first-name" id="first-name" autocomplete="given-name"
+                                    class="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-3">
+                            <label for="last-name" class="block text-sm font-light leading-6 text-gray-900">Last
+                                name *</label>
+                            <div class="mt-2">
+                                <input type="text" name="last-name" id="last-name" autocomplete="family-name"
+                                    class="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mt-10 space-y-10">
+                        <div class="sm:col-span-4">
+                            <label for="email" class="block text-sm font-light leading-6 text-gray-900">Email
+                                address *</label>
+                            <div class="mt-2">
+                                <input id="email" name="email" type="email" autocomplete="email"
+                                    class="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mt-10">
+                        <fieldset>
+                            <legend class="block text-sm font-light leading-6 text-gray-900">Query Type *</legend>
+                            <div class="flex space-x-4 mt-2">
+                                <div
+                                    class="flex-1 flex items-center gap-x-3 rounded-md border px-3 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+                                    <input id="push-everything" name="push-notifications" type="radio"
+                                        class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    <label for="push-everything"
+                                        class="flex-1 block text-sm font-light leading-6 text-gray-900">General
+                                        Enquiry</label>
+                                </div>
+                                <div
+                                    class="flex-1 flex items-center gap-x-3 rounded-md border px-3 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+                                    <input id="push-email" name="push-notifications" type="radio"
+                                        class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    <label for="push-email"
+                                        class="flex-1 block text-sm font-light leading-6 text-gray-900">Support
+                                        Request</label>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+
+                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div class="col-span-full">
+                            <label for="about" class="block text-sm font-light leading-6 text-gray-900">Message
+                                *</label>
+                            <div class="mt-2">
+                                <textarea id="about" name="about" rows="3"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mt-10">
+                        <fieldset>
+                            <div class="flex items-center">
+                                <div class="flex items-center gap-x-3">
+                                    <input id="comments" name="comments" type="checkbox"
+                                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    <div class="text-sm leading-6">
+                                        <p class="text-gray-500">I consent to being contacted by the team *</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+
+                    <div class="mt-6">
+                        <button type="submit"
+                            class="w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            Submit
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
---
+++ resources/views/contactDeux.blade.php
@@ -0,0 +1,99 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('contact') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    {{-- title of section  --}}
+
+                    <h1>Contact Us</h1>
+
+                    {{-- input first and last name --}}
+
+                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div class="sm:col-span-3">
+                            <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First
+                                name *</label>
+                            <div class="mt-2">
+                                <input type="text" name="first-name" id="first-name" autocomplete="given-name"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-3">
+                            <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last
+                                name *</label>
+                            <div class="mt-2">
+                                <input type="text" name="last-name" id="last-name" autocomplete="family-name"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+
+                        {{-- email input --}}
+
+                        <div class="sm:col-span-4">
+                            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email
+                                address</label>
+                            <div class="mt-2">
+                                <input id="email" name="email" type="email" autocomplete="email"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- button in input --}}
+
+                    <div class="flex flex-row">
+                        <fieldset>
+                            <legend class="text-sm font-semibold leading-6 text-gray-900">Query type
+                            </legend>
+                            <div class="mt-6 space-y-6">
+                                <div class="flex flex-1 items-center gap-x-3">
+                                    <input id="push-everything" name="push-notifications" type="radio"
+                                        class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    <label for="push-everything"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Everything</label>
+                                </div>
+                                <div class="flex flex-1 items-center gap-x-3">
+                                    <input id="push-email" name="push-notifications" type="radio"
+                                        class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    <label for="push-email"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Same as
+                                        email</label>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+
+                    {{-- message input --}}
+
+                    <div class="col-span-full">
+                        <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Message</label>
+                        <div class="mt-2">
+                            <textarea id="about" name="about" rows="3"
+                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                        </div>
+                    </div>
+                    <div class="relative flex gap-x-3">
+                        <div class="flex h-6 items-center">
+                          <input id="comments" name="comments" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                        </div>
+                        <div class="text-sm leading-6">
+                          <p class="text-gray-500">I consent to being contacted by the team</p>
+                        </div>
+                      </div>
+                      <div class="mt-6 flex items-center justify-end gap-x-6">
+                        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 w-full text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">submit</button>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</x-app-layout>
